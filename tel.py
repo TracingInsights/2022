48593c998626c49cb9fdc354b670729ab704caf3
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2023-12-24 20:40:41 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -23,8 +23,9 @@ events = [#'Pre-Season Test',
      # 'Australian Grand Prix', 'Emilia Romagna Grand Prix', 'Miami Grand Prix',
     # 'Spanish Grand Prix', 'Monaco Grand Prix', 'Azerbaijan Grand Prix',
     # 'Canadian Grand Prix', 'British Grand Prix', 'Austrian Grand Prix',
-    'French Grand Prix', 'Hungarian Grand Prix', 'Belgian Grand Prix', 
-    # 'Dutch Grand Prix', 'Italian Grand Prix', 'Singapore Grand Prix', 'Japanese Grand Prix', 'United States Grand Prix', 'Mexico City Grand Prix', 'São Paulo Grand Prix', 'Abu Dhabi Grand Prix'
+    # 'French Grand Prix', 'Hungarian Grand Prix', 'Belgian Grand Prix', 
+    'Dutch Grand Prix', 'Italian Grand Prix', 'Singapore Grand Prix', 
+    # 'Japanese Grand Prix', 'United States Grand Prix', 'Mexico City Grand Prix', 'São Paulo Grand Prix', 'Abu Dhabi Grand Prix'
 ]
          
 
